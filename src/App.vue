47ea649b6fcdbcc74da52f122855db2e32ef85c0
Jenna <jennometry@gmail.com>
2023-11-05 16:21:58 +1000

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import DueDate from './components/DueDate.vue';
 import LanguageForm from './components/LanguageForm.vue';
 import SpotifyButton from './components/SpotifyButton.vue';
-
+import Image from 'primevue/image';
 import { changeText, captureUserInput } from './js/input';
 import { initSpotify } from './js/spotify';
 
-//console.log(import.meta.env.VITE_OPENAI_API_KEY);
 const text = changeText();
 const inputs = captureUserInput();
 initSpotify(); // Provide the list of stuff 
-//await testSpotify();
 </script>
 
 <template>
+    <div class="content-center">
     <h1 class="text-3xl font-bold">
-    Hello World! Text is: {{ text.label }}
+    Dev-FM: Playlists perfectly suited for your sprint!
     </h1>
+
+    <br/>
     <DueDate></DueDate>
     <LanguageForm/>
+    <br/>
+    <br/>
     <SpotifyButton/>
+<br/>    
+    <Image width="150" src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Radio_svg_icon.svg"/>
+
+    </div>
 </template>
